8517ad7a3b0b0735fd46d2a1302331186430e6ff
Piotr Stepien <naz@o2.pl>
2021-02-01 12:48:06 +0100

    Add getting the titles of all articles

--- js/script.js
+++ js/script.js
@@ -60,6 +60,9 @@ function generateTitleLinks() {
     //console.log(articleId);
 
     /* find the title element */
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+    //console.log(articleTitle);
+
     /* get the title from the title element */
     /* create HTML of the link */
     /* insert link into titleList */
